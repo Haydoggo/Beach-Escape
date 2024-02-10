1c3fec43de60a5b3d0073370a2c6eddca2ba15b1
Hayden Leete <haydenleete@gmail.com>
2024-02-10 16:52:41 +1300

    add on hit attack to BaseUnit

--- App/Scenes/Entities/Units/BaseUnit.gd
+++ App/Scenes/Entities/Units/BaseUnit.gd
@@ -1,15 +1,16 @@
 class_name BaseUnit extends Node2D
 
 @export var unit_info : UnitInfo
-#@export var icon : Texture2D = preload("res://icon.svg")
 
 var path : Array[Vector2] = []
 var path_position := 0 # tracks the index of the path 
 var health : float
 
+
 func _ready() -> void:
 	path = get_path_points(global_position)
 	health = unit_info.health
+
 
 # override this method for path generation
 func get_path_points(origin : Vector2) -> Array[Vector2]:
@@ -17,6 +18,7 @@ func get_path_points(origin : Vector2) -> Array[Vector2]:
 	for point in unit_info.path:
 		points.append(point + origin)
 	return points
+
 
 func move():
 	path_position += 1
@@ -29,13 +31,13 @@ func move():
 	.set_trans(Tween.TRANS_CUBIC)
 
 
+func _on_hit(attack_packet):
+	health -= attack_packet.damage
+	if health <= 0:
+		queue_free()
+
+
 func _input(event: InputEvent) -> void:
 	if event is InputEventKey:
 		if event.pressed and event.keycode == KEY_M:
 			move()
-
-#func generate_icon():
-	#var new_icon = TextureRect.new()
-	#new_icon.size = Vector2(128,128)
-	#new_icon.texture = icon
-	#return new_icon
