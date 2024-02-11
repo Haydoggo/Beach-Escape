04e214e0471c6f858104c81cda7297665a6239ed
plexsoup <plexsoup@gmail.com>
2024-02-11 00:38:46 -0800

    bugfix intermission

--- App/Scenes/CutScenes/acquire_new_units.gd
+++ App/Scenes/CutScenes/acquire_new_units.gd
@@ -17,22 +17,27 @@ func _ready():
 func spawn_unit(unit_metadata):
 	var new_unit = load("res://App/Scenes/Entities/Units/Procreator.tscn").instantiate()
 	var screen_size = get_viewport_rect().size
-	new_unit.global_position = Vector2(randf_range(10, screen_size.x-10), randf_range(10, screen_size.y-10))
+	var unit_size = 128
+	new_unit.global_position = Vector2(randf_range(128, screen_size.x-unit_size), randf_range(10, screen_size.y-unit_size))
 	new_unit.activate(unit_metadata)
 	add_child(new_unit)
 
 func _process(_delta):
 	if State == States.READY:
 		if Engine.get_process_frames() % 30 == 0:
-			if get_remaining_pairs_count() == 0:
+			var remaining_pairs = get_remaining_pairs_count()
+			if remaining_pairs == 0:
 				Globals.load_next_level()
+			else:
+				$CanvasLayer/Control/PairsRemaining/RemainingPairCount.text = str(remaining_pairs)
+
 
 func get_remaining_pairs_count() -> int:
 	var remaining_pairs = 0
 	for unit_metadata in Globals.unit_metadata:
 		var remaining_procreators = 0
 		for procreator in get_tree().get_nodes_in_group("Procreators"):
-			if procreator.can_procreate():
+			if procreator.unit_info.name == unit_metadata.name and procreator.can_procreate():
 				remaining_procreators += 1
 		remaining_pairs += floor( float(remaining_procreators) / 2.0 )
 	return remaining_pairs
