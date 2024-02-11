5f9bcc70b8c24e7947496ec7ed2152e0880f2a28
Hayden Leete <haydenleete@gmail.com>
2024-02-11 16:19:51 +1300

    Updated critter paths to be relative

--- App/Scenes/Entities/Units/Urchin/Urchin.gd
+++ App/Scenes/Entities/Units/Urchin/Urchin.gd
@@ -1,19 +1,9 @@
 extends BaseUnit
 
 
-func get_path_points(origin : Vector2) -> Array[Vector2]:
-	return super(origin)
-	# create a straight line, one tile every index
-	var retval : Array[Vector2] = [origin]
-	for i in 100:
-		var next_pos = retval[-1] + Vector2(128, 0)
-		retval.append(next_pos)
-		retval.append(next_pos)
-	return retval
-
 func move():
 	super.move_next()
-	if (path_position % 2) == 0:
+	if (path_index % 2) == 0:
 		create_tween().tween_property(self, "scale:x", 0.5, 0.3).set_ease(Tween.EASE_IN_OUT)\
 		.set_trans(Tween.TRANS_CUBIC)
 	else:
