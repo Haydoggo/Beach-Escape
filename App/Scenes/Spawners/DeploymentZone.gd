1b433a1475b4d4f3f2d2b9fa22ae80b7cca5d21f
plexsoup <plexsoup@gmail.com>
2024-02-11 16:31:49 -0800

    deployment area WIP

---
+++ App/Scenes/Spawners/DeploymentZone.gd
@@ -0,0 +1,17 @@
+extends Area2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+
+
+
+func _on_mouse_entered():
+	$SelectedTileCursor.activate()
+
+
+func _on_mouse_exited():
+	$SelectedTileCursor.deactivate()
---
+++ App/Scenes/Spawners/selected_tile_cursor.gd
@@ -0,0 +1,24 @@
+extends Sprite2D
+
+var active : bool = false
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta):
+	if active:
+		var zone = get_parent()
+		var mouse_pos = get_global_mouse_position()
+		#var constraints : Rect2 = zone.get_node("CollisionShape2D").shape.get_rect()
+		#if constraints.has_point(mouse_pos):
+			#global_position = mouse_pos.snapped(Vector2(128,128))
+		global_position = mouse_pos.snapped(Vector2(128,128))
+		
+		
+func activate():
+	active = true
+func deactivate():
+	active = false
