be13332d74a88814383c22a8f91dc8520e1a9cf7
plexsoup <plexsoup@gmail.com>
2024-02-11 23:24:16 -0800

    add fishing hook bleed tower

---
+++ App/Scenes/Entities/Components/Weapons/persistent_stationary_weapon.gd
@@ -0,0 +1,26 @@
+extends Node2D
+
+@export var attack_packet : AttackPacket
+
+
+
+func _on_tick():
+	attack_units_in_my_zone()
+	
+func attack_units_in_my_zone():
+	var present_enemies = get_local_enemies()
+	for enemy in present_enemies:
+		if enemy.has_method("_on_hit"):
+			enemy._on_hit(attack_packet)
+			print(owner.name + " attacked " + enemy.name)
+	
+	
+func get_local_enemies():
+	var candidates = $Area2D.get_overlapping_areas()
+	var present_enemies = []
+	for candidate in candidates:
+		if candidate.is_in_group("Units"):
+			present_enemies = candidate
+		elif candidate.owner and candidate.owner.is_in_group("Units"):
+			present_enemies = candidate.owner
+	return present_enemies
