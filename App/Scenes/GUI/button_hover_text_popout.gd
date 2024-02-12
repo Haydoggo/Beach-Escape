0e39dddecbea87cd7718cbef486af1957ef545ff
plexsoup <plexsoup@gmail.com>
2024-02-12 14:52:59 -0800

    bugfixing popup info panel

--- App/Scenes/GUI/button_hover_text_popout.gd
+++ App/Scenes/GUI/button_hover_text_popout.gd
@@ -6,14 +6,19 @@ var original_position : Vector2
 func _ready():
 	original_position = position
 
-func popup(newText):
+func popup(newText : String):
+	if newText == null or newText.is_empty():
+		return
+	
 	$UnitDescription.text = newText
 	var tween = create_tween()
 	var height = get_rect().size.y + 174
+	tween.set_trans(Tween.TRANS_CUBIC).set_ease(Tween.EASE_IN_OUT)
 	tween.tween_property(self, "position", original_position + Vector2.UP * height, 0.2)
 
 func close():
-	$UnitDescription.text = ""
+	#$UnitDescription.text = ""
 	var tween = create_tween()
+	tween.set_trans(Tween.TRANS_CUBIC).set_ease(Tween.EASE_IN_OUT)
 	tween.tween_property(self, "position", original_position, 0.1)
 	
