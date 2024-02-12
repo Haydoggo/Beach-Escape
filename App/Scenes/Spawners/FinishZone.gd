e20bbd035c40aedec067a05ed36ed004cb16e541
Hayden Leete <haydenleete@gmail.com>
2024-02-13 00:41:53 +1300

    Add variable win condition

--- App/Scenes/Spawners/FinishZone.gd
+++ App/Scenes/Spawners/FinishZone.gd
@@ -1,10 +1,21 @@
 extends Area2D
 
-var finish_button = preload("res://App/Scenes/Spawners/finish_counter_icon.tscn")
+const finish_button = preload("res://App/Scenes/Spawners/finish_counter_icon.tscn")
 @export var next_scene_path = "res://App/Scenes/CutScenes/acquire_new_units.tscn"
 
 enum States { WAITING, LISTENING }
 var State = States.WAITING
+@onready var level : BaseLevel = owner
+var arrived_units = {}
+
+func _ready() -> void:
+	for unit_info in Globals.unit_metadata:
+		arrived_units[unit_info.name] = 0
+	for unit_count in level.required_units_for_win:
+		var new_unit_counter = finish_button.instantiate()
+		new_unit_counter.required_unit_count = unit_count.count
+		new_unit_counter.activate(unit_count.unit_info)
+		$CompletedUnitCounters.add_child(new_unit_counter)
 
 func _on_area_entered(area):
 	if area.owner != null and area.owner.is_in_group("Units"):
@@ -16,6 +27,7 @@ func _on_area_entered(area):
 				new_unit_counter.activate(unit.unit_info)
 				$CompletedUnitCounters.add_child(new_unit_counter)
 			$CompletedUnitCounters.get_node(unit.unit_info.name).add_unit()
+			arrived_units[unit.unit_info.name] += 1
 			
 		if area.owner.has_method("_on_finish_line_crossed"):
 			area.owner._on_finish_line_crossed()
@@ -24,11 +36,19 @@ func _process(_delta):
 	if State == States.LISTENING and Engine.get_process_frames() % 10 == 0:
 		var units_remaining = get_unit_count_on_field()
 		if units_remaining == 0:
-			print("Level Complete!")
-			SceneLoader.load_scene(next_scene_path)
+			if has_enough_units_to_win():
+				print("Level Complete!")
+				SceneLoader.load_scene(next_scene_path)
+			else:
+				print("Very sad, you lose :(((")
+				SceneLoader.reload_current_scene()
 		
 
-
+func has_enough_units_to_win() -> bool:
+	for required_unit_count in level.required_units_for_win:
+		if arrived_units[required_unit_count.unit_info.name] < required_unit_count.count:
+			return false
+	return true
 	
 
 func get_unit_count_on_field():
