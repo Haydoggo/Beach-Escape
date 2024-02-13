202c17bca6ff29a0eda59e71bf22911f2912a525
Hayden Leete <haydenleete@gmail.com>
2024-02-13 19:14:42 +1300

    Update mudfish for #14

--- App/Scenes/Entities/Units/Mudfish/Mudfish.gd
+++ App/Scenes/Entities/Units/Mudfish/Mudfish.gd
@@ -1,18 +1,15 @@
 extends BaseUnit
 
+const PIT_ATTACK = preload("res://App/Scenes/Entities/Towers/PitTrapAttack.tres")
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
 
 func move_forward(pos : Vector2):
 	super.move_forward(pos)
 	if path_index == 1:
-		if not is_near_finish_line():
-			tunnel_down()
+		tunnel_down()
 	elif path_index == 2:
 		global_position = pos
 		tunnel_up()
-
-func is_near_finish_line():
-	if global_position.x < get_viewport_rect().size.x - 256:
-		return true
 
 func attack_tower(tower):
 	if path_index == 2:
@@ -26,16 +23,20 @@ func on_blocked():
 	else:
 		super.on_blocked()
 
+func _on_hit(attack_packet : AttackPacket):
+	if attack_packet == PIT_ATTACK:
+		path_index = 1
+	else:
+		super._on_hit(attack_packet)
+
 func tunnel_down():
-	#$Hitbox.monitorable = false # <-- prevents finish line seeing you
+	animation_player.play("TunnelDown")
 	$Hitbox/CollisionShape2D.disabled = true
-	create_tween().tween_property(self, "modulate:a", 0.5, 0.5)
 
 func tunnel_up():
 	moisture = unit_info.moisture
 	moisture_indicator.moisture = moisture
 	var tween = create_tween()
-	tween.tween_property(self, "modulate:a", 1, 0.5)
-	await tween.finished
-	#$Hitbox.monitorable = true
+	animation_player.play("TunnelUp")
+	await animation_player.animation_finished
 	$Hitbox/CollisionShape2D.disabled = false
