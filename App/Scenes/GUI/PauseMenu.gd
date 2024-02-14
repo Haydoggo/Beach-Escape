af09a10a12bf213a0806f08cc1a4bedf72cacc64
plexsoup <plexsoup@gmail.com>
2024-02-13 19:25:16 -0800

    add pause menu

---
+++ App/Scenes/GUI/PauseMenu.gd
@@ -0,0 +1,18 @@
+extends Panel
+
+func _ready():
+	hide()
+
+
+func _unhandled_input(event):
+	if not visible and Input.is_action_just_pressed("ui_cancel"):
+		self.show()
+		get_tree().set_pause(true)
+	elif visible and Input.is_action_just_pressed("ui_cancel"):
+		self.hide()
+		get_tree().set_pause(false)
+
+
+func _on_return_to_game_pressed():
+	self.hide()
+	get_tree().set_pause(false)
