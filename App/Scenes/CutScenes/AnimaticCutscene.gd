9b05bd33979a14ea742db88aef8ff60749371e29
plexsoup <plexsoup@gmail.com>
2024-02-13 19:09:44 -0800

    add delay to tower hover text popup

--- App/Scenes/CutScenes/AnimaticCutscene.gd
+++ App/Scenes/CutScenes/AnimaticCutscene.gd
@@ -17,19 +17,24 @@ func _on_animation_player_animation_finished(anim_name):
 		#if next_scene_path != "":
 			#SceneLoader.load_scene(next_scene_path)
 
-func _unhandled_input(_event):
+func _input(event):
 	if State == States.RUNNING:
 		if Input.is_action_just_pressed("ui_cancel"):
 			State = States.ENDING
 			$AnimationPlayer.seek(10.8, true)
 		elif Input.is_action_just_pressed("ui_accept"):
-			var seek_points = [ 0, 3.5, 6, 8, 9.5, 10.8 ]
-			var current_point = 0
-			for i in seek_points.size():
-				if $AnimationPlayer.current_animation_position > seek_points[i]:
-					current_point = i
-			if current_point +1 >= seek_points.size()-1:
-				State = States.ENDING
-			else:
-				$AnimationPlayer.seek(seek_points[min(current_point+1, seek_points.size()-1)])
-			
+			seek_next_keyframe()
+		elif event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT:
+			if event.is_pressed():
+				seek_next_keyframe()
+				
+func seek_next_keyframe():
+	var seek_points = [ 0, 3.5, 6, 8, 9.5, 10.8 ]
+	var current_point = 0
+	for i in seek_points.size():
+		if $AnimationPlayer.current_animation_position > seek_points[i]:
+			current_point = i
+	if current_point +1 >= seek_points.size()-1:
+		State = States.ENDING
+	else:
+		$AnimationPlayer.seek(seek_points[min(current_point+1, seek_points.size()-1)])
--- App/Scenes/Entities/Components/hover_popup_info.gd
+++ App/Scenes/Entities/Components/hover_popup_info.gd
@@ -1,8 +1,18 @@
 extends Node2D
 
-var hovering : bool = false
+var mouse_over : bool = false
+var time_at_hover_start : int = 0 #msec
+var hover_popup_delay : int = 300 #msec
+
 signal hovered
 signal mouse_exited
+
+enum States { IDLE, LISTENING, HOVERING }
+var State = States.IDLE :
+	set(value):
+		State = value
+		#$StateLabel.text = States.keys()[value]
+
 
 func _ready():
 	await get_tree().create_timer(0.5).timeout
@@ -13,12 +23,29 @@ func delayed_ready():
 	hovered.connect(Globals.current_level._on_tower_hovered)
 	mouse_exited.connect(Globals.current_level._on_tower_mouse_exited)
 
+func _process(_delta):
+	if State == States.LISTENING:
+		if mouse_over and Time.get_ticks_msec() > time_at_hover_start + hover_popup_delay:
+			if owner and owner.get("description"):
+				hovered.emit(owner.description)
+				State = States.HOVERING
+
+
 func _on_area_2d_mouse_entered():
-	hovering = true
-	if owner and owner.get("description"):
-		hovered.emit(owner.description)
+	if State == States.IDLE:
+		mouse_over = true
+		time_at_hover_start = Time.get_ticks_msec()
+		State = States.LISTENING
+	
+
 
 func _on_area_2d_mouse_exited():
-	hovering = false
-	if owner and owner.get("description"):
-		mouse_exited.emit()
+	mouse_over = false
+	if State == States.HOVERING:
+		if owner and owner.get("description"):
+			mouse_exited.emit()
+		State = States.IDLE
+	elif State == States.LISTENING:
+		State = States.IDLE
+		
+	
