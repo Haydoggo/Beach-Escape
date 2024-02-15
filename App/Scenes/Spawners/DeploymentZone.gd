9a1f1c0b9e67b2b1d0bb1cf6bf333d5ad6a7bfbe
plexsoup <plexsoup@gmail.com>
2024-02-15 00:09:37 -0800

    make deployment zone look like water?

--- App/Scenes/Spawners/DeploymentZone.gd
+++ App/Scenes/Spawners/DeploymentZone.gd
@@ -6,7 +6,13 @@ func _ready():
 	pass # Replace with function body.
 
 
-
+func expand(play_space_size, deployment_area_width, tile_size):
+	var collision_shape = $CollisionShape2D
+	collision_shape.shape.size = Vector2(deployment_area_width * tile_size, play_space_size.y * tile_size)
+	collision_shape.position = collision_shape.shape.size / 2.0
+	$ColorRect.size = collision_shape.shape.size
+	$DeploymentZonePanel.size = collision_shape.shape.size
+	
 
 
 func _on_mouse_entered():
