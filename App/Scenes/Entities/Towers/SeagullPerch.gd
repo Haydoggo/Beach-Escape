e0490a03a4d3c336b35bb4a4513e6fa94b575a1c
Hayden Leete <haydenleete@gmail.com>
2024-02-15 20:01:08 +1300

    Update Seagull behaviour

---
+++ App/Scenes/Entities/Towers/SeagullPerch.gd
@@ -0,0 +1,28 @@
+@tool
+class_name SeagullPerch extends Node2D
+
+const PERCHES_GROUP = &"SeagullPerches"
+const SEAGULL_SCENE = preload("res://App/Scenes/Entities/Towers/SeagullTrap.tscn")
+@onready var label: Label = $PanelContainer/Label
+
+func _ready() -> void:
+	deferred_ready.call_deferred()
+
+func deferred_ready():
+	if Engine.is_editor_hint():
+		get_tree().call_group(PERCHES_GROUP, "update_number")
+	else:
+		$PanelContainer.hide()
+		print("I am %s, first is %s" % [self, get_tree().get_first_node_in_group(PERCHES_GROUP)])
+		if self == get_tree().get_first_node_in_group(PERCHES_GROUP):
+			var seagull = SEAGULL_SCENE.instantiate()
+			add_sibling(seagull)
+
+
+func _exit_tree() -> void:
+	if Engine.is_editor_hint() and get_parent() is TileMap:
+		remove_from_group(PERCHES_GROUP)
+		get_tree().call_group(PERCHES_GROUP, "update_number")
+
+func update_number():
+	label.text = str(get_tree().get_nodes_in_group(PERCHES_GROUP).find(self) + 1)
--- App/Scenes/Entities/Towers/SeagullTrap.gd
+++ App/Scenes/Entities/Towers/SeagullTrap.gd
@@ -1,140 +1,102 @@
-@tool
-
 extends Node2D
 
-@export var description : String = "Seagull moves between tiles. Dangerous."
-
 enum States { IDLE, MOVING, WAITING, DYING, DEAD }
-var State = States.MOVING
+enum TravelPatterns {PING_PONG, LOOP}
+
+## Can manually add SeagullPerches to this property in case you want multiple seagulls with their own perchs
+@export var perches : Array[SeagullPerch]
+@export var travel_pattern : TravelPatterns
+@export var description : String = "Seagull moves between tiles. Dangerous."
 @export var speed = 150.0
-var direction : int = 1
 @export var attack_packet : AttackPacket
 @export var health_max : float = 50.0
-var health = health_max
-
-enum travel_types { WALK, FLY }
-@export var travel_type : travel_types = travel_types.WALK
-
-var ticks : int = 0
-var tick_at_last_stop : int = 0
-var tick_at_start_move : int = 0
-@export var move_steps_without_attacking : int = 2
+@export var flight_dist_per_tick : int = 10000
 @export var tick_delay_between_moves : int = 2
 
-var seagull_position_last_frame : Vector2
+var State = States.IDLE
+var direction : int = 1
+var health = health_max
+var ticks_spent_waiting = 0
+var perch_index = 0
+var target_perch : SeagullPerch = null
+
+@onready var seagull_sprite: AnimatedSprite2D = $Seagull
+@onready var health_component: Node2D = $Components/HealthComponent
+@onready var attack_area: Area2D = $AttackArea
+@onready var hitbox_shape: CollisionShape2D = $Hitbox/CollisionShape2D
+
 
 
 func _ready():
-	if Engine.is_editor_hint():
-		colorize_seagulls()
-	else:
-		hide_waypoints()
-		await get_tree().create_timer(0.5).timeout
-		delayed_ready()
+	if perches.is_empty():
+		perches.append_array(get_tree().get_nodes_in_group(SeagullPerch.PERCHES_GROUP))
+	assert(perches.size() > 1, "Seagull must have more than one perch")
+	global_position = perches[0].global_position
+	select_next_perch()
 
-
-
-func delayed_ready():
-	# wait for other birds to arrive in the scene
-	build_path()
-	idle()
-	update_health_bar()
-
-
-func colorize_seagulls():
-	if not is_top_bird():
-		set_modulate(Color.DARK_OLIVE_GREEN)
-	else:
-		set_modulate(Color.WHITE)
-
-
-func is_top_bird():
-	var top_bird = true
-	var seagulls = get_tree().get_nodes_in_group("Seagulls").duplicate()
-	seagulls.erase(self)
-	for seagull in seagulls:
-		if is_instance_valid(seagull) and seagull.get_index() < get_index():
-			top_bird = false
-	return top_bird
-
-
-func build_path():
-	if is_top_bird():
-		var curve = Curve2D.new()
-		
-		var waypoints = get_tree().get_nodes_in_group("Seagulls")
-		for waypoint in waypoints:
-			curve.add_point(to_local(waypoint.global_position))
-		$Path2D.curve = curve
-
-func hide_waypoints():
-	if not is_top_bird():
-		visible = false
-	else:
-		visible = true
 
 func idle():
 	State = States.IDLE
-	$Path2D/PathFollow2D/Seagull.play("idle")
-	
+	seagull_sprite.play("idle")
+	hitbox_shape.disabled = false
+	ticks_spent_waiting = 0
 
-
-func can_attack():
-	var ready_and_able = true
-	if State in [States.DYING, States.DEAD]:
-		ready_and_able = false
-	return ready_and_able
-	
-	
-func _on_area_2d_area_entered(area):
-	if can_attack():
-		if area.owner != null and area.owner.is_in_group("Units"):
-			if area.owner.has_method("_on_hit"):
-				area.owner._on_hit(attack_packet)
-				idle()
-				
 
 func _on_hit(attackPacket):
 	if not State in [ States.DYING, States.DEAD ]:
-		health -= attackPacket.damage
-		update_health_bar()
-		if health <= 0:
-			begin_dying()
+		health_component._on_hit(attackPacket)
 
-
-func update_health_bar():
-	var health_bar = $Path2D/PathFollow2D/Seagull/Components/HealthBar
-	health_bar.max_value = health_max
-	health_bar.value = health
 
 func begin_dying():
-	$Path2D/PathFollow2D/Seagull.play("die")
+	seagull_sprite.play("die")
 	State = States.DYING
 	await get_tree().create_timer(5).timeout
 	queue_free()
 
+
 func move_next():
+	var perch = perches[perch_index]
+	seagull_sprite.play("fly")
+	hitbox_shape.disabled = true
 	
-	$Path2D/PathFollow2D/Seagull.play("fly")
+	var target_position = global_position.move_toward(target_perch.global_position, flight_dist_per_tick)
 	
-	var block_size = 128
-	
-	var tween = create_tween()
-	#var path = $Path2D
-	var progress = $Path2D/PathFollow2D.progress
-	tween.tween_property($Path2D/PathFollow2D, "progress", progress+(direction * block_size), 0.3).set_ease(Tween.EASE_IN_OUT)\
-	.set_trans(Tween.TRANS_CUBIC)
+	var tween = create_tween().set_ease(Tween.EASE_IN_OUT).set_trans(Tween.TRANS_CUBIC)
+	tween.tween_property(self, "global_position", target_position, 0.3)
 	tween.tween_callback(_on_move_end)
 
 
 func _on_move_end():
-	$Path2D/PathFollow2D/Seagull.play("idle")
-	if direction == 1 and $Path2D/PathFollow2D.get_progress_ratio() > 0.9:
-		direction = -1
-	elif direction == -1 and $Path2D/PathFollow2D.get_progress_ratio() < 0.1:
-		direction = 1
+	if global_position == target_perch.global_position:
+		select_next_perch()
+		idle()
+
+
+func select_next_perch():
+	perch_index += direction
+	if travel_pattern == TravelPatterns.PING_PONG:
+		if perch_index == perches.size()-1:
+			direction = -1
+		if perch_index == 0:
+			direction = 1
+	if travel_pattern == TravelPatterns.LOOP:
+		perch_index %= perches.size()
+	target_perch = perches[perch_index]
+
+
+func attack():
+	for area in attack_area.get_overlapping_areas():
+		if area.owner != null and area.owner.is_in_group("Units"):
+			if area.owner.has_method("_on_hit"):
+				area.owner._on_hit(attack_packet)
+
 
 func _on_tick():
-	if is_top_bird():
-		ticks += 1
+	if State == States.IDLE and ticks_spent_waiting >= tick_delay_between_moves:
+		State = States.MOVING
+	
+	if State == States.MOVING:
 		move_next()
+	if State == States.IDLE:
+		ticks_spent_waiting += 1
+		attack()
