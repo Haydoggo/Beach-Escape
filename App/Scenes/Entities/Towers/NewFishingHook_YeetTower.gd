c4ab4eda3fc1e70c79373ce7ca48399a0b816904
plexsoup <plexsoup@gmail.com>
2024-02-16 14:17:55 -0800

    arcade mode fishing hooks go away after a while

--- App/Scenes/Entities/Towers/NewFishingHook_YeetTower.gd
+++ App/Scenes/Entities/Towers/NewFishingHook_YeetTower.gd
@@ -1,7 +1,12 @@
+## Fishing hook "tower". May be spawned on level start or at intervals.
+## In arcade mode only: should go away after a certain number of ticks.
 extends "res://App/Scenes/Entities/Towers/StaticTrap_OnTick.gd"
 
 var direction = Vector2(2,-1)
 var speed : float = 250.0
+
+var duration_in_ticks : int = 10
+var ticks_elapsed : int = 0
 
 func _ready():
 	$Sprite2D/FishSprite.hide()
@@ -15,9 +20,24 @@ func _process(delta):
 func is_in_viewport():
 	return get_viewport_rect().has_point(global_position)
 
+
+func reel_in():
+	var tween = create_tween()
+	tween.tween_property(self, "global_position", Vector2(get_viewport_rect().size.x + 128, -128), 0.3125)
+	
+
+
 func _on_attacked():
 	State = States.DISABLED
 	var tween = create_tween()
 	tween.tween_property($Sprite2D/FishSprite, "rotation", PI/2.0, 0.3)
 	
+
+	
+func _on_tick():
+	super()
+	ticks_elapsed += 1
+	if Globals.game_mode == Globals.game_modes.ARCADE:
+		if ticks_elapsed > duration_in_ticks:
+			reel_in()
 	
